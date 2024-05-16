b618862a230f33739eb0815786af23b0fe331d0a
sun20886 <ssun20886@gmail.com>
2024-05-16 20:41:11 +0900

    timer update (setTimeout(), setInterval(), clearInterval())

--- D-Day-Counter/script.js
+++ D-Day-Counter/script.js
@@ -1,5 +1,6 @@
 const messageContainer = document.querySelector('#d-day-message');
 const container = document.querySelector("#d-day-container");
+const intervalIdArr = [];
 
 container.style.display = 'none';
 messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요</h3>";
@@ -24,6 +25,7 @@ const counterMaker = function () {
         container.style.display = 'none';
         messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>"
         messageContainer.style.display = 'flex';
+        setClearInterval();
         return;
 
     } else if (isNaN(remaining)) {
@@ -31,6 +33,7 @@ const counterMaker = function () {
         container.style.display = 'none';
         messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>"
         messageContainer.style.display = 'flex';
+        setClearInterval();
         return;
     }
 
@@ -52,7 +55,27 @@ const counterMaker = function () {
 };
 
 const starter = function () {
+
     container.style.display = 'flex'
     messageContainer.style.display = 'none';
-    counterMaker();
+
+    //100번까지만 반복 가능
+    // for (let i = 0; i < 100; i++) {
+    //     setTimeout(counterMaker, 1000 * i);
+    // }
+
+    //setInterval()이 1초 뒤에 실행되기 때문에 counterMaker() 한 번 먼저 실행
+    counterMaker()
+    //1초마다 counterMaker()함수 실행. 1초 뒤에 실행
+    const intervalId = setInterval(counterMaker, 1000);// return값은 반복되는 코드 interval의 id값
+    intervalIdArr.push(intervalId);
+};
+
+const setClearInterval = function () {
+    container.style.display = 'none';
+    messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요</h3>";
+    messageContainer.style.display = 'flex';
+    for (let i = 0; i < intervalIdArr.length; i++) {
+        clearInterval(intervalIdArr[i]);
+    }
 };
