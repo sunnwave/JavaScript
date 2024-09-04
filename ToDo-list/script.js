9f08d3a4f5aa3b84c44ed87132d0c8d76746697e
sun20886 <ssun20886@gmail.com>
2024-09-04 18:49:47 +0900

    todo list

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,14 +1,66 @@
 const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector("#todo-list");
+
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+
+if (savedTodoList) {
+    for (i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
+    }
+}
+
+const createTodo = function (storageData) {
+
+    let todoContents = todoInput.value;
+    if (storageData) {
+        todoContents = storageData.contents
+    }
+
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
+
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+        saveItemsFunc();
+    });
+
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+        saveItemsFunc();
+    });
+
+    newSpan.textContent = todoContents;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = '';
+    saveItemsFunc();
+}
 
 const keyCodeCheck = function () {
     if (window.event.keyCode === 13 && todoInput.value != '') {
-        const todoList = document.querySelector("#todo-list");
-        const newLi = document.createElement('li');
-        const newSpan = document.createElement('span');
-
-        newSpan.textContent = todoInput.value;
-        newLi.appendChild(newSpan);
-        todoList.appendChild(newLi);
-        todoInput.value = '';
+        createTodo();
     }
+};
+
+const deleteAll = function () {
+    const liList = document.querySelectorAll('li');
+    for (let i = 0; i < liList.length; i++) {
+        liList[i].remove();
+    }
+}
+
+const saveItemsFunc = function () {
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++) {
+        const todoObj = {
+            contents: todoList.children[i].querySelector("span").textContent,
+            complete: todoList.children[i].classList.contains('complete')
+        };
+        saveItems.push(todoObj);
+    }
+
+    // console.log(typeof (JSON.stringify(saveItems)));
+    localStorage.setItem('saved-items', JSON.stringify(saveItems));
 };
