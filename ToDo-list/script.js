e1c101ef8458df595c0b22c488fe6f26b6dc88ee
sunnwave <ssun20886@gmail.com>
2024-09-17 18:32:27 +0900

    imple: open weather map API 연결 구현
    - geolocation()으로 위도, 경도 정보 받아open weather map API 연결
    - apikey.js에 open weather map API appid 저

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,78 +1,101 @@
-const todoInput = document.querySelector('#todo-input');
+const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
+const API_KEY = config.apikey;
 
-const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
 const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
 
-    let todoContents = todoInput.value;
-    if (storageData) {
-        todoContents = storageData.contents
-    }
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
 
-    const newLi = document.createElement('li');
-    const newSpan = document.createElement('span');
-    const newBtn = document.createElement('button');
-
-    newBtn.addEventListener('click', () => {
-        newLi.classList.toggle('complete');
-        saveItemsFunc();
-    });
-    
-    newLi.addEventListener('dblclick', () => {
-        newLi.remove();
-        saveItemsFunc();
-    });
-    
-    if(storageData?.complete){
-        newLi.classList.add('complete')
-    }
-    
-    newSpan.textContent = todoContents;
-    newLi.appendChild(newBtn);
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = '';
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
     saveItemsFunc();
-}
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFunc();
+  });
+
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFunc();
+};
 
 const keyCodeCheck = function () {
-    if (window.event.keyCode === 13 && todoInput.value != '') {
-        createTodo();
-    }
+  if (window.event.keyCode === 13 && todoInput.value != "") {
+    createTodo();
+  }
 };
 
 const deleteAll = function () {
-    const liList = document.querySelectorAll('li');
-    for (let i = 0; i < liList.length; i++) {
-        liList[i].remove();
-    }
-    saveItemsFunc();
-}
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+  saveItemsFunc();
+};
 
 const saveItemsFunc = function () {
-    const saveItems = [];
-    for (let i = 0; i < todoList.children.length; i++) {
-        const todoObj = {
-            contents: todoList.children[i].querySelector("span").textContent,
-            complete: todoList.children[i].classList.contains('complete')
-        };
-        saveItems.push(todoObj);
-    }
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
 
-    saveItems.length===0
-        ?localStorage.removeItem('saved-items')
-        :localStorage.setItem('saved-items', JSON.stringify(saveItems))
-
-    // if(saveItems.length===0){
-    //     localStorage.removeItem('saved-items')
-    // }else{
-    //     localStorage.setItem('saved-items', JSON.stringify(saveItems));
-    // }
-}
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
 
 if (savedTodoList) {
-    for (i = 0; i < savedTodoList.length; i++) {
-        createTodo(savedTodoList[i]);
-    }
+  for (i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
 }
+
+const weatherSearch = function (position) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${API_KEY}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].description);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const accessToGeo = function (position) {
+  const positionObj = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {});
+};
+
+askForLocation();
